bee2f8bd244eb1272ba8f5d154f520e9fb2a4386
Russel Dsouza <russel.dsouza47@gmail.com>
2020-05-21 08:25:20 +0530

    Update

--- applescripts/tunes.js
+++ applescripts/tunes.js
@@ -3,17 +3,17 @@ if (Application("Music").running()) {
     const track = Application("Music").currentTrack;
     const artist = track.artist();
     const title = track.name();
-    output = `${title} - ${artist}`.substr(0, 50);
+    output = `${title} by ${artist}`.substr(0, 50);
 } else if (Application("iTunes").running()) {
     const track = Application("iTunes").currentTrack;
     const artist = track.artist();
     const title = track.name();
-    output = `${title} - ${artist}`.substr(0, 50);
+    output = `${title} by ${artist}`.substr(0, 50);
 } else if (Application("Spotify").running()) {
     const track = Application("Spotify").currentTrack;
     const artist = track.artist();
     const title = track.name();
-    output = `${title} - ${artist}`.substr(0, 50);
+    output = `${title} by ${artist}`.substr(0, 50);
 }
 
 output;
